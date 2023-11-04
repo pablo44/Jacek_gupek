b161cbb05866c4bc3ae700ac5a4678a1141ca400
pablo44 <pawelrz@wp.pl>
2023-11-04 15:57:56 +0100

    Decorator and it's wrapper

--- Jacek.py
+++ Jacek.py
@@ -7,30 +7,32 @@ class YatzyGameClass:
     def __init__(self):
         self.ds = [Die(), Die(), Die(), Die(),Die()]
         self.goOn = True
+        self.intTurn = 0
 
 
     def yatzy_go(f):
-        while YatzyGameClass.goOn == True:
-            YatzyGameClass.intTurn = 0
+        #while YatzyGameClass.goOn == True:
+            #YatzyGameClass.intTurn = 0
+# here we should have some start and stop option in order to control the function my_yatzy
+        #flag = True
+        #initialise flag as a boolean variable to control the loop logic and becomes as " stop button"
+        def wrapper(self):
+            flag = True
 
-
-        flag = bool
-        def wrapper(*YatzyGameClass:Die()):
-            flag == True
-
-
-            for j in range(1,5):
-                if YatzyGameClass.ds[j].value = not self.ds[j-1]value:
-                    flag == False
+# here comes while loop to wrapp the conditions of the game:
+            while self.intTurn < 3:
+                for j in range(1,5):
+                    if YatzyGameClass.ds[j].value != YatzyGameClass.ds[j-1].value:
+                        flag == False
                 if flag == True:
-                    print(f'You got Yatzy in {YatzyGameClass[0].value}s')
+                    print(f'You got Yatzy in {YatzyGameClass.ds[0].value}s')
                     return
-                elif YatzyGameClass.intTurn =! 2:
+                elif YatzyGameClass.intTurn != 2:
                     if input('want to throw again?( "y" for yes anything else for no)') == 'y':
                         YatzyGameClass.intTurn =+ 1
                     else:
 
-                        YatzyGameClass.goOn = not YatzyGameClass.goOn
+                        YatzyGameClass.goOn != YatzyGameClass.goOn
                         break
                 else:
                     if input( 'Game Over want to play again?')== 'y':
@@ -46,7 +48,7 @@ class YatzyGameClass:
 
     
 
-    #creating en decorator in order to reuse it in a another function to modifie game conditions 
+    #creating en decorator in order to reuse this func my_yatzy in a another function-yatzy_go to modifie game conditions inuti this function's wrapper
     @yatzy_go
     def my_yatzy(self):
         while self.goOn == True:
@@ -58,5 +60,11 @@ class YatzyGameClass:
                 for i, d in enumerate(self.ds, 1):
                     print(f'{i}, {d}')
             return YatzyGameClass.my_yatzy
+        
+def main():
+    YatzyGameClass()
+    if __name__ == '__main__':
+        main()
+
 
 
