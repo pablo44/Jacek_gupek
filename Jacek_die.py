310dd4ab6d514dcc468248514d07853173a3afa5
pablo44 <pawelrz@wp.pl>
2023-11-13 10:51:27 +0100

    last descr

--- Jacek_die.py
+++ Jacek_die.py
@@ -4,6 +4,7 @@ import random
 
 class Die:
 #we get out of functionality in __int__ and keep it in func Die_roll not to cofuse in the main code and avoid duble running diffrent functionalities
+#Die_roll fixes the confusion from roll_start function that got error while reading first random turns from __init__ instance method and Die_roll at the same time
     def __init__(self):
         self.value = None
 
